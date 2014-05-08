0cbbcbe1dc58579308e01f537aefa2d3835ce510
Steven Weinberger <steven@psycomp.com>
2014-05-08 15:52:37 -0400

    Initial load

---
+++ HMC58X3/HMC58X3.h
@@ -0,0 +1,29 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _HMC58X3_H_
+#define _HMC58X3_H_
+#include "Arduino.h"
+//add your includes for the project HMC58X3 here
+
+
+//end of add your includes here
+#ifdef __cplusplus
+extern "C" {
+#endif
+void loop();
+void setup();
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+//add your function definitions for the project HMC58X3 here
+
+
+
+
+//Do not add code below this line
+#endif /* _HMC58X3_H_ */
