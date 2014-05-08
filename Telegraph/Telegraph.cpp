1fbdfda3ee621b42680d12309640976d3b67424d
Steven Weinberger <steven@psycomp.com>
2014-05-08 16:05:45 -0400

    Initial Load

---
+++ Telegraph/Telegraph.cpp
@@ -0,0 +1,13 @@
+// Do not remove the include below
+#include "Telegraph.h"
+
+Morse morse(13);
+
+void setup() {}
+
+void loop() {
+  morse.dot(); morse.dot(); morse.dot();
+  morse.dash(); morse.dash(); morse.dash();
+  morse.dot(); morse.dot(); morse.dot();
+  delay(3000);
+}
